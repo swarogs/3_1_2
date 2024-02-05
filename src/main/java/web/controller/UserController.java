0075c111551ab62975c3d208e0f32a37e8f19b76
swarogs <swarog664@gmail.com>
2024-02-05 20:15:26 +0300

    2 Commit

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -15,54 +15,54 @@ import java.util.List;
 @Controller
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
 
-	@Autowired
-	public UserController(UserService usersService) {
-		this.userService = usersService;
-	}
+    @Autowired
+    public UserController(UserService usersService) {
+        this.userService = usersService;
+    }
 
-	@RequestMapping(value = "/")
-	public String getAllUsers(Model model) {
-		List<User> allUsers = userService.getAllUsers();
-		model.addAttribute("allUsers", allUsers);
-		return "all_users";
-	}
+    @RequestMapping(value = "/")
+    public String getAllUsers(Model model) {
+        List<User> allUsers = userService.getAllUsers();
+        model.addAttribute("allUsers", allUsers);
+        return "all_users";
+    }
 
-	@GetMapping("/creat_user")
-	public String creatUserFrom(Model model) {
-		model.addAttribute("user", new User());
-		return "creat_user";
-	}
+    @GetMapping("/creat_user")
+    public String creatUserFrom(Model model) {
+        model.addAttribute("user", new User());
+        return "creat_user";
+    }
 
-	@PostMapping("/creat_user")
-	public String addUser(@ModelAttribute("user") User user1) {
-		userService.addUser(user1);
-		return "redirect:/";
-	}
+    @PostMapping("/creat_user")
+    public String addUser(@ModelAttribute("user") User user1) {
+        userService.addUser(user1);
+        return "redirect:/";
+    }
 
-	@GetMapping("/update_user")
-	public String updateUserFrom(Model model) {
-		model.addAttribute("userU", new User());
-		return "update_user";
-	}
+    @GetMapping("/update_user")
+    public String updateUserFrom(Model model) {
+        model.addAttribute("userU", new User());
+        return "update_user";
+    }
 
-	@PostMapping("/update_user")
-	public String updateUser(@ModelAttribute("userU") User user) {
-		userService.updateUser(user.getId(), user);
-		return "redirect:/";
-	}
+    @PostMapping("/update_user")
+    public String updateUser(@ModelAttribute("userU") User user) {
+        userService.updateUser(user.getId(), user);
+        return "redirect:/";
+    }
 
-	@GetMapping("/delete_user")
-	public String deleteUserFrom(Model model) {
-		model.addAttribute("userD", new User());
-		return "delete_user";
-	}
+    @GetMapping("/delete_user")
+    public String deleteUserFrom(Model model) {
+        model.addAttribute("userD", new User());
+        return "delete_user";
+    }
 
-	@PostMapping("/delete_user")
-	public String deleteUser(@ModelAttribute("userD") User user) {
-		userService.deleteUser(user.getId());
-		return "redirect:/";
-	}
+    @PostMapping("/delete_user")
+    public String deleteUser(@ModelAttribute("userD") User user) {
+        userService.deleteUser(user.getId());
+        return "redirect:/";
+    }
 
 }
